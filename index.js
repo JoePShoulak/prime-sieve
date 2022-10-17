3d8de010b59551d5c0cfdab0f58398136242a002
Joe P Shoulak <joe@squarecompass.io>
2022-10-17 00:00:12 -0500

    made it actually usable

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-/* A Sieve of Eratosthenes (https://en.wikipedia.org/wiki/Sieve_of_Eratosthene) 
-   algorithm that I put together because I was bored. Check to see if a number
-   is prime in O(n log log n) time! */
+import Sieve from "./lib/Sieve.js";
+import Inquirer from "inquirer"
 
-const floor = Math.floor;
-const sqrt = Math.sqrt;
-
-/* == HELPER FUNCTIONS == */
-const multiplesUpTo = (num, limit) => {
-    const countingArray = [...Array(limit).keys()]; // [1, 2, 3, 4, ...]
-    return countingArray.map(n => (n+1)*num).slice(1); // [-3-, 6, 9, 12, ...]
-}
-
-/* == CLASS == */
-class Sieve {
-    #composites;
-    #current;
-
-    constructor(target) {
-        this.#composites = new Set();
-        this.#current = 2;
-        this.prime = undefined;
-        this.target = target;
-    }
-
-    #nextPrime = () => {
-        do { this.#current++ } while (this.#composites.has(this.#current));
-    }
-
-    #markComposites = () => {
-        const limit = floor(this.target / this.#current); 
-
-        multiplesUpTo(this.#current, limit).forEach(n => this.#composites.add(n));
-    }
-
-    compute = () => {
-        while (this.#current < sqrt(this.target)) {
-            this.#markComposites();
-            this.#nextPrime();
-        }
-
-        this.prime = !this.#composites.has(this.target);
-
-        return this;
+const questions = {
+    getNumber: {
+        name: "number",
+        type: "number",
+        message: "What number do you want to test?"
     }
 }
 
-console.log(new Sieve(7919).compute().prime);
+const init = async () => {
+    console.log("Welcome to the prime number sieve!")
+
+    const number = (await Inquirer.prompt(questions.getNumber)).number
+
+    console.log(new Sieve(number).compute().prime);
+}
+
+init()
